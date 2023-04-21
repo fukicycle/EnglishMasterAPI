9a829491d06ad5c2002fb3a5158c38517613446f
Fuki Sato <106070646+fukicycle@users.noreply.github.com>
2023-04-21 14:02:38 +0900

    レベル取得

---
+++ EnglishMasterAPI/Controllers/LevelController.cs
@@ -0,0 +1,40 @@
+﻿using EnglishMasterAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishMasterAPI.Controllers
+{
+    [Route("api/[action]/[controller]")]
+    [ApiController]
+    public class LevelController : ControllerBase
+    {
+        private DB _db;
+        public LevelController(DB db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(new ResultContent<List<Level>>
+                {
+                    Message = "Success",
+                    Content = _db.Levels.ToList(),
+                    StatusCode = System.Net.HttpStatusCode.OK
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new ResultContent<string>
+                {
+                    Message = "Internal Server Error",
+                    Content = ex.Message,
+                    StatusCode = System.Net.HttpStatusCode.InternalServerError
+                });
+            }
+        }
+    }
+}
