5d800b44fa065c67725d5a7a1465d493d7945879
Fuki Sato <106070646+fukicycle@users.noreply.github.com>
2023-04-21 10:44:09 +0900

    Add models

---
+++ EnglishMasterAPI/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnglishMasterAPI.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            ExamResults = new HashSet<ExamResult>();
+            PracticeResults = new HashSet<PracticeResult>();
+        }
+
+        public long Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Icon { get; set; }
+
+        public virtual ICollection<ExamResult> ExamResults { get; set; }
+        public virtual ICollection<PracticeResult> PracticeResults { get; set; }
+    }
+}
