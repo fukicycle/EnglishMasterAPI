7f1e9053701376f3e1182a086a44537198a559c3
Fuki Sato <106070646+fukicycle@users.noreply.github.com>
2023-04-21 11:54:59 +0900

    Vocabularyを追加する処理を実装 各パラメータを確認し無効な値が入っている場合ははじく

--- EnglishMasterAPI/Controllers/VocabularyController.cs
+++ EnglishMasterAPI/Controllers/VocabularyController.cs
@@ -15,7 +15,86 @@ namespace EnglishMasterAPI.Controllers
             _db = db;
         }
 
-
+        [HttpPost]
+        public IActionResult Post([FromHeader] string token, List<VocabularyData> vocabularyDatas)
+        {
+            try
+            {
+                var auth = _db.Users.FirstOrDefault(a => a.Token == token);
+                if (auth is null)
+                {
+                    return Ok(new ResultContent<string>
+                    {
+                        Message = "Authentication failed.",
+                        Content = "Invalid credential",
+                        StatusCode = System.Net.HttpStatusCode.Unauthorized
+                    });
+                }
+                var partOfSpeeches = _db.PartOfSpeeches.ToList();
+                if (vocabularyDatas.Any(a => !partOfSpeeches.Select(b => b.Id).Contains(a.PartOfSpeechID)))
+                {
+                    string invalidIds = string.Join(",", vocabularyDatas.Where(a => !partOfSpeeches.Select(b => b.Id).Contains(a.PartOfSpeechID)).Select(a => a.PartOfSpeechID).ToList());
+                    return Ok(new ResultContent<string>
+                    {
+                        Message = "Validation failed.",
+                        Content = $"Invalid PartOfSpeechID is found. Invalid id:{invalidIds}",
+                        StatusCode = System.Net.HttpStatusCode.BadRequest
+                    });
+                }
+                var levels = _db.Levels.ToList();
+                if (vocabularyDatas.Any(a => !levels.Select(b => b.Id).Contains(a.LevelID)))
+                {
+                    string invalidIds = string.Join(",", vocabularyDatas.Where(a => !levels.Select(b => b.Id).Contains(a.LevelID)).Select(a => a.LevelID).ToList());
+                    return Ok(new ResultContent<string>
+                    {
+                        Message = "Validation failed.",
+                        Content = $"Invalid LevelID is found. Invalid id:{invalidIds}",
+                        StatusCode = System.Net.HttpStatusCode.BadRequest
+                    });
+                }
+                int count = 0;
+                int wordCount = 0;
+                foreach (var vocaburalyData in vocabularyDatas)
+                {
+                    Word? word = _db.Words.FirstOrDefault(a => a.Word1.ToUpper().Equals(vocaburalyData.Word.ToUpper()));
+                    if (word is null)
+                    {
+                        word = new Word
+                        {
+                            Word1 = vocaburalyData.Word.ToLower()
+                        };
+                        _db.Words.Add(word);
+                        _db.SaveChanges();
+                        wordCount++;
+                    }
+                    Vocabulary vocabulary = new Vocabulary
+                    {
+                        PartOfSpeechId = vocaburalyData.PartOfSpeechID,
+                        LevelId = vocaburalyData.LevelID,
+                        WordId = word.Id,
+                        Meaning = vocaburalyData.Meaning
+                    };
+                    _db.Vocabularies.Add(vocabulary);
+                    count++;
+                }
+                _db.SaveChanges();
+                return Ok(new ResultContent<string>
+                {
+                    Message = "Success",
+                    Content = $"Adding {count} vocabularies and {wordCount} words.",
+                    StatusCode = System.Net.HttpStatusCode.Created
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new ResultContent<string>
+                {
+                    Message = "Internal Server Error",
+                    Content = ex.Message,
+                    StatusCode = System.Net.HttpStatusCode.InternalServerError
+                });
+            }
+        }
 
     }
 }
---
+++ EnglishMasterAPI/Models/VocabularyData.cs
@@ -0,0 +1,10 @@
+﻿namespace EnglishMasterAPI.Models
+{
+    public class VocabularyData
+    {
+        public long PartOfSpeechID { get; set; }
+        public string Word { get; set; } = null!;
+        public string Meaning { get; set; } = null!;
+        public long LevelID { get; set; }
+    }
+}
